cca0785451bc9067abca16a13788c55fa3e0d8d9
Tom Hummel <tohu@hey.com>
2021-09-27 22:36:06 -0700

    add first food test

--- test.js
+++ test.js
@@ -627,3 +627,43 @@ tap.test('avoid head to head: two safe options (#2)', (t) => {
 
   t.end()
 })
+
+tap.test('eat food if hungry', (t) => {
+  const you = {
+    head: { x: 5, y: 5 },
+    body: [
+      { x: 5, y: 5 },
+      { x: 5, y: 6 },
+      { x: 5, y: 7 }
+    ],
+    health: 30
+  }
+
+  const game = {
+    board: {
+      height: 11,
+      width: 11,
+      snakes: [
+        {
+          id: 'id',
+          name: 'us',
+          health: 30,
+          length: 3,
+          head: you.head,
+          body: you.body,
+          latency: 0,
+          shout: '',
+          squad: ''
+        }
+      ],
+      food: [{ x: 4, y: 5 }],
+      hazards: []
+    },
+    you: you
+  }
+
+  const result = move(game)
+  const note = 'eat the food to the left'
+  t.equal(result.move, 'left', note)
+  t.end()
+})
