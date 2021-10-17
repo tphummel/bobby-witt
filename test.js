2f21cf2a37de94459f0ca4f29e7e04f3a14cb0a5
Tom Hummel <tohu@hey.com>
2021-10-17 14:39:08 -0700

    wrote failing test for attacking smaller snake head to head
    status quo is to avoid head to head conflict

--- test.js
+++ test.js
@@ -667,3 +667,67 @@ tap.test('eat food if hungry', (t) => {
   t.equal(result.move, 'left', note)
   t.end()
 })
+
+tap.test('attack smaller snake, head to head', (t) => {
+  const you = {
+    head: { x: 5, y: 5 },
+    body: [
+      { x: 5, y: 5 },
+      { x: 5, y: 6 },
+      { x: 5, y: 7 },
+      { x: 6, y: 7 },
+      { x: 7, y: 7 }
+    ],
+    health: 77
+  }
+
+  const opp = {
+    head: { x: 5, y: 3 },
+    body: [
+      { x: 5, y: 3 },
+      { x: 5, y: 2 },
+      { x: 5, y: 1 },
+      { x: 4, y: 1 }
+    ],
+    health: 88
+  }
+
+  const game = {
+    board: {
+      height: 11,
+      width: 11,
+      snakes: [
+        {
+          id: 'id',
+          name: 'us',
+          health: you.health,
+          length: you.body.length,
+          head: you.head,
+          body: you.body,
+          latency: 0,
+          shout: '',
+          squad: ''
+        },
+        {
+          id: 'id2',
+          name: 'them',
+          health: opp.health,
+          length: opp.body.length,
+          head: opp.head,
+          body: opp.body,
+          latency: 0,
+          shout: '',
+          squad: ''
+        }
+      ],
+      food: [],
+      hazards: []
+    },
+    you: you
+  }
+
+  const result = move(game)
+  const note = 'attack the smaller snake'
+  t.equal(result.move, 'down', note)
+  t.end()
+})
